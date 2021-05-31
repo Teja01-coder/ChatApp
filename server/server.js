ba4616936c387b12d84aef2e2ca203c3b378e7f4
Sai Teja <saikunfu@gmail.com>
2021-06-01 00:38:56 +0530

    secondary commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const path = require("path");
+const http=require("http");
+const socketIO=require("socket.io");
 const publicPath=path.join(__dirname,'/../public');
 const port=process.env.PORT || 3000 ;
 const express=require("express");
-var app=express();
+let app=express();
+let server=http.createServer(app);
+let io=socketIO(server);
 app.use(express.static(publicPath));
 
 
@@ -11,10 +15,17 @@ app.use(express.static(publicPath));
 
 
 
+io.on('connection',function(socket){
+  console.log(" A new user just Connected");
+
+  socket.on('disconnect',function(){
+    console.log("the user is disconnected");
+  });
+});
 
 
 
 
-app.listen(port,function(){
+server.listen(port,function(){
   console.log("the server is live");
 })

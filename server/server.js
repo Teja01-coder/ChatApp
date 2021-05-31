e24b4e5727d734cccbcf0eff669af2a4052350b8
Sai Teja <saikunfu@gmail.com>
2021-05-31 23:38:31 +0530

    skeliton of server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const publicPath=path.join(__dirname,'/../public');
+const port=process.env.PORT || 3000 ;
+const express=require("express");
+var app=express();
+app.use(express.static(publicPath));
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,function(){
+  console.log("the server is live");
+})
